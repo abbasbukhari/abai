64ee5b1e383d4101da311ef1a4b8c14d6eabf936
abbas <abbasbukhari2015@gmail.com>
2026-05-03 14:41:47 -0400

    Fix CSS import order: tech2.css after site.css for correct nav positioning with sysbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/base.css";
+import "./styles/site.css";
 import "./styles/tech.css";
 import "./styles/tech2.css";
-import "./styles/site.css";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
